ecf804e7300f39f642a23cfc38e7f1f0afcf7420
Трифонов Алексей Андреевич <222659@edu.fa.ru>
2024-04-03 09:38:43 +0300

    Добавление сложения чисел для программы Calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,15 @@
 # Функция сложения чисел
 def calc(x, y):
     return x+y
+
+print("Выберите действие:")
+print("Сложение")
+
+choice = input("Введите номер")
+
+num1 = float(input("Введите первое число"))
+num2 = float(input("Введите второе число"))
+if choice == '1':
+    print("Результат равен: ", calc(num1, num2))
+else:
+    print("Неправильный выбор действия")
